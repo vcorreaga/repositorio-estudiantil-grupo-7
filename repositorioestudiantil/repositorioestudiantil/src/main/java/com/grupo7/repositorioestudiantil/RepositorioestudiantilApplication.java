46233840745fa1302ab158298659bd580ed8e92e
Mariana20209 <mariasuarez20209@gmail.com>
2026-02-22 22:06:26 -0500

    Estructura base del proyecto

---
+++ repositorioestudiantil/repositorioestudiantil/src/main/java/com/grupo7/repositorioestudiantil/RepositorioestudiantilApplication.java
@@ -0,0 +1,13 @@
+package com.grupo7.repositorioestudiantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioestudiantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioestudiantilApplication.class, args);
+	}
+
+}
---
+++ repositorioestudiantil/repositorioestudiantil/src/test/java/com/grupo7/repositorioestudiantil/RepositorioestudiantilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo7.repositorioestudiantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioestudiantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
